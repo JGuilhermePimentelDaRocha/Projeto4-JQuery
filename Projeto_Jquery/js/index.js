ba04a46f60be4eeab95ce508726a3e2b9d0b1003
Jose Guilherme <joseguilhermepimenteldarocha@gmail.com>
2021-04-01 08:43:18 -0300

    Aualizações finais
    Atualizações Finais e criação de um PPT para apresentação do projeto.

--- Projeto_Jquery/js/index.js
+++ Projeto_Jquery/js/index.js
@@ -122,26 +122,6 @@ $(document).ready(function () {
         }
     });
 
-    // animação
-
-    /*
-    function animeScroll() {
-        var disTopoScroll = $(document).scrollTop();
-        console.log(disTopoScroll);
-        $(".anime_inicial").each(function () {
-            itemTopo = $(this).offset().top
-            console.log(itemTopo);
-            if (disTopoScroll > itemTopo - 150) {
-                $(this).addClass("anime_final");
-            } else {
-                $(this).removeClass("anime_final");
-            }
-        })
-    }
-    animeScroll();
-    $(window).scroll(function () {
-        animeScroll();
-    })*/
 
     // declarações - Acessibilidade visual
 
